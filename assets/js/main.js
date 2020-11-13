fe72d4cc219ad5e2f30aa1744c2fad9269789b9a
matteo <matteoezio@me.com>
2020-11-13 14:25:27 +0100

    aggiunto scheletro js
    sono state aggiunte solo le domande di base

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// Nome utente
+var userName = prompt("Salve, qual è il suo nome?");
+console.log(userName);
+
+// e l'età del passeggero.
+var userAge = parseInt(prompt("Gnetilissim* " + userName + " In che anno è nato?"));
+console.log(userAge);
+
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+var userKm = prompt("" + userName + " quanti Km intendi percorrere?");
+console.log(userKm);
+
+
+// va applicato uno sconto del 20% per i minorenni
+
+
+// va applicato uno sconto del 40% per gli over 65
+
+
+
+
+
+
+
+
+
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+//  il prezzo del biglietto è definito in base ai km (0.21 € al km)
